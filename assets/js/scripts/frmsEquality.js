855350370335c8d848d410798df9531ab605ff49
Jorge Malagon <jorge.malagon@roll.mx>
2021-04-27 22:08:12 -0500

    Se agrega nueevas secciones restantes

--- assets/js/scripts/frmsEquality.js
+++ assets/js/scripts/frmsEquality.js
@@ -71,4 +71,69 @@ jQuery(document).ready(function($){
     //   }
     // }
   });
+
+  // Formulario servicios
+  $("#serviciosfrm").validate({
+    errorElement: "div",
+    errorClass: "error-line",
+    rules: {
+      nombreServFrm: "required",
+      telServFrm: {
+        required: true,
+        number: true,
+      },
+      emailServFrm: {
+        required: true,
+        email: true    
+      },
+      mensajeServFrm: "required",
+      acceptServFrm: "required",
+    },
+    messages: {
+      nombreServFrm: "<i class='fa fa-exclamation-triangle'><i>",
+      telServFrm: {
+        required: "<i class='fa fa-exclamation-triangle'><i>",
+        number: "<i class='fa fa-exclamation-triangle'><i>",
+      },
+      emailServFrm: {
+        required: "<i class='fa fa-exclamation-triangle'><i>",
+        email: "<i class='fa fa-exclamation-triangle'><i>"    
+      },
+      mensajeServFrm: "<i class='fa fa-exclamation-triangle'><i>",
+      acceptServFrm: "<i class='fa fa-exclamation-triangle'><i>",
+    },
+    // submitHandler: function(form) {
+    //   var dataForm = $('#formContacto').serialize();
+    //   if (grecaptcha.getResponse() == ''){
+    //     $( '#reCaptchaError' ).html( '<p>Por favor, debes verificar el reCaptcha</p>' ).fadeOut(5000);
+
+    //   } else {
+    //   $.ajax({
+    //     url: $urlSitio + 'contactoScript.php',
+    //     type: 'POST',
+    //     data: dataForm,
+    //     beforeSend: function(xhr) {
+    //       $('.btnSend').addClass('loadBtn');
+    //     },
+    //     complete: function(xhr, textstatus) {
+    //       $('.btnSend').removeClass('loadBtn');
+    //     },
+    //     success: function(data) {
+    //       console.log(data);
+    //       $("#formContacto").each (function(){
+    //         this.reset();
+    //       });
+    //       $('#modalCongrats').removeClass('hideMo').addClass('showMo');
+    //       setTimeout(function(){
+    //         $('#modalCongrats').removeClass('showMo').addClass('hideMo');
+    //       }, 5000);
+    //       grecaptcha.reset();
+    //     },
+    //     error: function(e) {
+    //       console.log(e);
+    //     }
+    //   });
+    //   }
+    // }
+  });
 });
